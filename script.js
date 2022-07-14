147749e3448cb0ce15486695481e2b60b20485a0
irao12 <ivanrao@outlook.com>
2022-07-14 12:02:40 -0400

    added player's ability to place down their sign

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+// Module for the GameBoard
+const GameBoard = (() => {
+    //private
+    const _boardArray = new Array(9).fill('');
+
+    //public
+    const setSquare = (index, sign) => {
+        _boardArray.splice(index, 1, sign);
+    }
+
+    const getSquare = (index) => {
+        return _boardArray[index];
+    }
+
+    return {
+        setSquare, getSquare
+    };
+})();
+
 //Module for the Display Controller
 const DisplayController = ( ()=> {
     const playButton = document.querySelector('.play-button');
@@ -5,6 +24,8 @@ const DisplayController = ( ()=> {
     const playerVsPlayer = document.querySelector('.pvp');
     const playerVsAI = document.querySelector('.pva');
     const board = document.querySelector('.board');
+
+    const squares = document.querySelectorAll('.square');
     
     const _removePlayButton = () => {
         playButton.classList.remove('active-block');
@@ -15,40 +36,40 @@ const DisplayController = ( ()=> {
         choiceButtons.classList.remove('active-flex');
     }
 
+    const refreshBoard = () => {
+        squares.forEach((square)=>{
+            const index = square.index;
+            square.textContent = GameBoard.getSquare(index);
+        });
+    }
+
     const initBoard = () => {
        _removePlayButton();
        _removeChoiceButtons();
     }
 
     return {
-        playButton, choiceButtons, playerVsPlayer, playerVsAI, board,
-        initBoard: initBoard
+        playButton, choiceButtons, playerVsPlayer, playerVsAI, board, squares,
+        initBoard: initBoard,
+        refreshBoard: refreshBoard
     }
 })();
 
-// Module for the GameBoard
-const GameBoard = (() => {
-    //private
-    const _boardArray = new Array(9).fill(0, '');
-    //public
-    const setSquare = (index, sign) => {
-        _boardArray.splice(index, 1, sign);
-    }
-
-    return {
-        setSquare
-    };
-})();
 
 //Factory function for the players
-const Player = (sign='') => {
+const Player = (sign='', mode) => {
     let _sign = sign;
+    let _mode = mode;
 
     const setSign = (sign) => {_sign = sign};
     const getSign = () => {return _sign};
+    const makeMove = (index) => {
+        GameBoard.setSquare(index, _sign);
+        DisplayController.refreshBoard();
+    };
 
     return {
-        setSign, getSign
+        setSign, getSign, makeMove
     }
 }
 
@@ -57,6 +78,7 @@ const GameController = (() => {
 
     //private
     let _mode = "pvp";
+    let _currentTurn;
 
     const _changeMode = (button) => {
         const selectedMode = button.target.classList.contains('pvp') ? 'pvp' : 'pva';
@@ -67,12 +89,27 @@ const GameController = (() => {
 
         _mode = selectedMode;
     }
-    //public
+
+    const _initGame = () => {
+        DisplayController.initBoard();
+        const playerOne = Player('x', 'player');
+        const playerTwo = Player('o', (_mode == 'pvp') ? 'player' : 'AI');
+        _currentTurn = playerOne;
+
+        const squares = DisplayController.squares;
+        for (let i = 0; i < squares.length; i++){
+            squares[i].index = i;
+            squares[i].addEventListener('click', (e)=> {
+                _currentTurn.makeMove(e.target.index);
+                _currentTurn = _currentTurn === playerOne ? playerTwo : playerOne;
+            });
+        }
+    }
     
+    //public
 
     // setup
-    DisplayController.playButton.addEventListener('click', DisplayController.initBoard);
+    DisplayController.playButton.addEventListener('click', _initGame);
     DisplayController.playerVsPlayer.addEventListener('click', _changeMode);
     DisplayController.playerVsAI.addEventListener('click', _changeMode);
 })();
-
