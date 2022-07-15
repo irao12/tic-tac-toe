c041258b1bdde003689e19d5ad2912c479e97b87
irao12 <ivanrao@outlook.com>
2022-07-15 00:22:48 -0400

    added an algorithm to figure out if someone won and a reset button

--- script.js
+++ script.js
@@ -3,6 +3,66 @@ const GameBoard = (() => {
     //private
     const _boardArray = new Array(9).fill('');
 
+    const _hasVertical = (index, sign) => {
+        // check if there's a win vertically
+
+        // check if the squares above have the same sign
+        let squareAbove = index - 3;
+        while (squareAbove >= 0) {
+            // if the square above has the same sign
+            if (_boardArray[squareAbove] != sign){
+                return false;
+            }
+            squareAbove -= 3;
+        }
+
+        // check if the squares below have the same sign
+        let squareBelow = index + 3;
+        while (squareBelow <= 8) {
+            if (_boardArray[squareBelow] != sign){
+                return false;
+            }
+            squareBelow += 3;
+        }
+        return true;
+    }
+
+    const _hasHorizontal = (index, sign) => {
+        // check if there's a win horizontally
+        const row = Math.floor(index / 3);
+        const mostLeft = row * 3;
+        const mostRight = row * 3 + 2;
+
+        // check if the squares to the left are the same
+        let squareLeft = index - 1;
+        while (squareLeft >= mostLeft){
+            // check if the square to the left has the same sign
+            if (_boardArray[squareLeft] != sign){
+                return false;
+            }
+            squareLeft--;
+        }
+
+        let squareRight = index + 1;
+        while (squareRight <= mostRight){
+            // check if the square to the left has the same sign
+            if (_boardArray[squareRight] != sign){
+                return false;
+            }
+            squareRight++;
+        }
+        return true;
+    }
+
+    const _hasDiagonal = (sign) => {
+        // diagonal from the top right
+        let firstDiagonal = _boardArray[0] == _boardArray[4] && _boardArray[0] == _boardArray[8] && _boardArray[0] == sign;
+
+        let secondDiagonal = _boardArray[2] == _boardArray[6] && _boardArray[2] == _boardArray[4] && _boardArray[2] == sign;
+
+        return firstDiagonal || secondDiagonal;
+    }
+
     //public
     const setSquare = (index, sign) => {
         _boardArray.splice(index, 1, sign);
@@ -12,8 +72,30 @@ const GameBoard = (() => {
         return _boardArray[index];
     }
 
+    const isFull = () => {
+        for (let i = 0; i < _boardArray.length; i++){
+            if (_boardArray[i] == ''){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    const hasWin = (index, sign) => {
+        let vertical = _hasVertical(index, sign);
+        let horizontal = _hasHorizontal(index, sign);
+        let diagonal = _hasDiagonal(sign);
+
+        console.log(vertical, horizontal, diagonal);
+        return (vertical || horizontal || diagonal);
+    }
+
+    const resetBoard = () => {
+        _boardArray.fill('');
+    }
+
     return {
-        setSquare, getSquare
+        setSquare, getSquare, isFull, hasWin, resetBoard
     };
 })();
 
@@ -25,6 +107,7 @@ const DisplayController = ( ()=> {
     const playerVsAI = document.querySelector('.pva');
     const board = document.querySelector('.board');
     const announcement = document.querySelector('.announcement');
+    const resetButton = document.querySelector('.reset-button');
 
     const squares = document.querySelectorAll('.square');
     
@@ -44,20 +127,21 @@ const DisplayController = ( ()=> {
         });
     }
 
-    const changeAnnouncement = (currentTurn) => {
+    const changeTurnAnnouncement = (currentTurn) => {
         announcement.textContent = 'Player ' + currentTurn.getSign() + '\'s Turn';
     }
 
     const initBoard = () => {
        _removePlayButton();
        _removeChoiceButtons();
+       resetButton.classList.add('active-block');
     }
 
     return {
-        playButton, choiceButtons, playerVsPlayer, playerVsAI, board, squares,
+        playButton, choiceButtons, playerVsPlayer, playerVsAI, board, squares, resetButton,
         initBoard: initBoard,
         refreshBoard: refreshBoard,
-        changeAnnouncement
+        changeTurnAnnouncement
     }
 })();
 
@@ -96,24 +180,46 @@ const GameController = (() => {
         _mode = selectedMode;
     }
 
-    const _isDone = () => {
-         
+    const _isDone = (index) => {
+        const sign = _currentTurn.getSign();
+        if (GameBoard.hasWin(index, sign)) {
+            return "won";
+        }
+        else if (GameBoard.isFull()){
+            return "tie";
+        }   
+        return "";
     }
 
     const _initGame = () => {
         DisplayController.initBoard();
+       
         const playerOne = Player('X', 'player');
         const playerTwo = Player('O', (_mode == 'pvp') ? 'player' : 'AI');
+        // X always goes first
         _currentTurn = playerOne;
 
         function initMove(e) {
             const index = e.target.index;
+
+            // if square is empty, make the move, otherwise, ignore
             if (GameBoard.getSquare(index) == ''){
                 _currentTurn.makeMove(index);
+
+                if (_isDone(index) == 'won'){
+                    console.log(_currentTurn.getSign() + ' won')
+                }
+
                 _currentTurn = _currentTurn === playerOne ? playerTwo : playerOne;
-                _isDone();
-                DisplayController.changeAnnouncement(_currentTurn);
+                DisplayController.changeTurnAnnouncement(_currentTurn);
             }
+        }
+
+        function _resetGame () {
+            GameBoard.resetBoard();
+            _currentTurn = playerOne;
+            DisplayController.refreshBoard();
+            DisplayController.changeTurnAnnouncement(_currentTurn);
         }
 
         const squares = DisplayController.squares;
@@ -121,12 +227,14 @@ const GameController = (() => {
             squares[i].index = i;
             squares[i].addEventListener('click', initMove);
         }
+
+        DisplayController.changeTurnAnnouncement(_currentTurn);
+        DisplayController.resetButton.addEventListener('click', _resetGame);
     }
-    
-    //public
 
     // setup
     DisplayController.playButton.addEventListener('click', _initGame);
     DisplayController.playerVsPlayer.addEventListener('click', _changeMode);
     DisplayController.playerVsAI.addEventListener('click', _changeMode);
+
 })();
